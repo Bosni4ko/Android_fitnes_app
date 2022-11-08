5de386222e51711794fec3438f78d68d5c79d87a
Bosni4ko <stefanbosnyak9@gmail.com>
2022-11-08 13:34:13 +0200

    first commit

---
+++ app/src/main/java/com/coursework/fitnessapp/models/Workout.java
@@ -0,0 +1,14 @@
+package com.coursework.fitnessapp.models;
+
+import java.time.Period;
+import java.util.Date;
+
+public class Workout {
+    private String id;
+    private String name;
+    private Exercise[] exercises;
+    private Date dateTime;
+    private Period length;
+    private String status;
+
+}
