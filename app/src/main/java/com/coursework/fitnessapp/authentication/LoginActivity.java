0af9e17f27cfa9d2b6e6491d852b79423079d533
Bosni4ko <stefanbosnyak9@gmail.com>
2022-11-29 14:28:50 +0200

    Login

--- app/src/main/java/com/coursework/fitnessapp/authentication/LoginActivity.java
+++ app/src/main/java/com/coursework/fitnessapp/authentication/LoginActivity.java
@@ -1,16 +1,86 @@
 package com.coursework.fitnessapp.authentication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.strictmode.FragmentStrictMode;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.coursework.fitnessapp.MainActivity;
 import com.coursework.fitnessapp.R;
+import com.coursework.fitnessapp.models.ExerciseModel;
+import com.coursework.fitnessapp.supportclasses.TimeDuration;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private static final String TAG = "LoginActivity";
+    private SignInButton signInButton;
+    private TextInputLayout signInLayout;
+    ActivityResultLauncher<Intent> activityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                handleSignInResult(task);
+            }
+        });
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null){
+            goToApp();
+        }
+        signInLayout = findViewById(R.id.signInLayout);
+        signInButton = findViewById(R.id.signInBtn);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+                GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                activityResultLauncher.launch(signInIntent);
+
+            }
+
+        });
+
+    }
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //Log.e(TAG,"handleSignInResult" + account.getEmail());
+            goToApp();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            //Log.e(TAG, "signInResult:failed code=" + e.getMessage());
+            signInLayout.setError(getResources().getString(R.string.login_failed));
+        }
+    }
+    private void goToApp(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent );
     }
 }
