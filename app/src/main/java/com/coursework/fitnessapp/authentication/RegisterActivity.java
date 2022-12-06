c5b318f988bb1336efedeae7c7bf410f778fbc1a
Bosni4ko <stefanbosnyak9@gmail.com>
2022-12-06 11:25:45 +0200

    Register user layout

---
+++ app/src/main/java/com/coursework/fitnessapp/authentication/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.coursework.fitnessapp.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coursework.fitnessapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
