ab3cf66d33d2ff6a77866fd8a0d20d5166a01738
Bosni4ko <83237714+Bosni4ko@users.noreply.github.com>
2022-12-08 10:17:28 +0200

    No login/registration

--- app/src/main/java/com/coursework/fitnessapp/MainActivity.java
+++ app/src/main/java/com/coursework/fitnessapp/MainActivity.java
@@ -1,18 +1,23 @@
 package com.coursework.fitnessapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 
+import android.view.MenuItem;
 import android.view.View;
 
 
 import com.coursework.fitnessapp.DataBaseHelper.DataBaseHelper;
 import com.coursework.fitnessapp.workout.CreateWorkoutActivity;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -22,7 +27,9 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.coursework.fitnessapp.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.getMenu().getItem(3).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                logout();
+                return false;
+            }
+        });
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
@@ -45,6 +59,22 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(binding.navView, navController);
 
     }
-
+    private void logout(){
+        AlertDialog.Builder builder =  new AlertDialog.Builder(this);
+        builder.setTitle(R.string.logout)
+                .setMessage(R.string.logout_message)
+                .setNegativeButton(R.string.logout, new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseAuth.getInstance().signOut();
+                    }
+                }).setNeutralButton(R.string.back, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        builder.setCancelable(true);
+                    }
+                }).show();
+    }
 
 }
--- app/src/main/java/com/coursework/fitnessapp/authentication/LoginActivity.java
+++ app/src/main/java/com/coursework/fitnessapp/authentication/LoginActivity.java
@@ -4,31 +4,48 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.strictmode.FragmentStrictMode;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.coursework.fitnessapp.MainActivity;
 import com.coursework.fitnessapp.R;
 import com.coursework.fitnessapp.models.ExerciseModel;
 import com.coursework.fitnessapp.supportclasses.TimeDuration;
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String TAG = "LoginActivity";
+    private TextInputLayout credentialsLayout;
     private SignInButton googleSignInButton;
-    private TextInputLayout signInLayout;
     ActivityResultLauncher<Intent> activityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +57,14 @@ public class LoginActivity extends AppCompatActivity {
             public void onActivityResult(ActivityResult result) {
                 Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
                 handleSignInResult(task);
+
             }
         });
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         if(account != null){
             goToApp();
         }
-        signInLayout = findViewById(R.id.signInLayout);
-        googleSignInButton = findViewById(R.id.googleSignInBtn);
-        googleSignInButton.setSize(SignInButton.SIZE_STANDARD);
+        initLayout();
         googleSignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,16 +74,21 @@ public class LoginActivity extends AppCompatActivity {
                 GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
                 Intent signInIntent = googleSignInClient.getSignInIntent();
                 activityResultLauncher.launch(signInIntent);
-
             }
 
         });
 
     }
+    private void initLayout(){
+        credentialsLayout = findViewById(R.id.credentialsLayout);
+        googleSignInButton = findViewById(R.id.googleSignInBtn);
+        googleSignInButton.setSize(SignInButton.SIZE_STANDARD);
+    }
+
+
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-
             // Signed in successfully, show authenticated UI.
             //Log.e(TAG,"handleSignInResult" + account.getEmail());
             goToApp();
@@ -75,7 +96,7 @@ public class LoginActivity extends AppCompatActivity {
             // The ApiException status code indicates the detailed failure reason.
             // Please refer to the GoogleSignInStatusCodes class reference for more information.
             //Log.e(TAG, "signInResult:failed code=" + e.getMessage());
-            signInLayout.setError(getResources().getString(R.string.login_failed));
+            credentialsLayout.setError(getResources().getString(R.string.login_failed));
         }
     }
     private void goToApp(){
@@ -83,4 +104,5 @@ public class LoginActivity extends AppCompatActivity {
         intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent );
     }
+
 }
